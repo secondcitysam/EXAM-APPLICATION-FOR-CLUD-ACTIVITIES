4de851935c591859bcb93707c13a734b1447d176
secondcitysam <samyak.kedari4444@gmail.com>
2025-08-13 19:57:29 +0530

    f

---
+++ admin_material/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
+from admin_material.forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserSetPasswordForm, UserPasswordChangeForm
+from django.contrib.auth import logout
+
+from home.models import Student, CollegeSubjects, College
+
+
+# Create your views here.
+
+# Pages
+def index(request):
+  College1 = College.objects.all()
+  collen = 0
+  for Col in College1:
+    collen += 1
+
+  CollegeSubjects1 = CollegeSubjects.objects.all()
+  colsublen = 0
+  for ColSub in CollegeSubjects1:
+    colsublen += 1
+
+  Student1 = Student.objects.all()
+  studlen = 0
+  for stud1 in Student1:
+    studlen += 1
+
+  context = {
+    "collen": collen,
+    "colsublen": colsublen,
+    "studlen ": studlen,
+    "College": College1,
+    "ColSub": CollegeSubjects1,
+  }
+  return render(request, 'pages/index.html', context,{ 'segment': 'index' })
+
+def billing(request):
+  return render(request, 'pages/billing.html', { 'segment': 'billing' })
+
+def tables(request):
+  return render(request, 'pages/tables.html', { 'segment': 'tables' })
+
+def vr(request):
+  return render(request, 'pages/virtual-reality.html', { 'segment': 'vr' })
+
+def rtl(request):
+  return render(request, 'pages/rtl.html', { 'segment': 'rtl' })
+
+def notification(request):
+  return render(request, 'pages/notifications.html', { 'segment': 'notification' })
+
+def profile(request):
+  return render(request, 'pages/profile.html', { 'segment': 'profile' })
+
+
+# Authentication
+class UserLoginView(LoginView):
+  template_name = 'accounts/login.html'
+  form_class = LoginForm
+
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      print('Account created successfully!')
+      return redirect('/accounts/login/')
+    else:
+      print("Register failed!")
+  else:
+    form = RegistrationForm()
+
+  context = { 'form': form }
+  return render(request, 'accounts/register.html', context)
+
+def logout_view(request):
+  logout(request)
+  return redirect('/accounts/login/')
+
+class UserPasswordResetView(PasswordResetView):
+  template_name = 'accounts/password_reset.html'
+  form_class = UserPasswordResetForm
+
+class UserPasswordResetConfirmView(PasswordResetConfirmView):
+  template_name = 'accounts/password_reset_confirm.html'
+  form_class = UserSetPasswordForm
+
+class UserPasswordChangeView(PasswordChangeView):
+  template_name = 'accounts/password_change.html'
+  form_class = UserPasswordChangeForm
